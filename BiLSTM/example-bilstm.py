c00a36aa1416328d6ddba549dfc9c88b337c786c
Saif <droconos99@gmail.com>
2025-03-28 23:58:53 +0600

    Update: BiLSTM

--- BiLSTM/example-bilstm.py
+++ BiLSTM/example-bilstm.py
@@ -3,9 +3,7 @@ from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Bidirectional, LSTM, Dense, Embedding
 
 # Sample Data (Batch of 3 sequences, each with 5 words, vocab size 1000)
-X = np.random.randint(1000, size=(3, 5))
 
-# BiLSTM Model
 model = Sequential([
     Embedding(input_dim=1000, output_dim=64, input_length=5),  # Word embeddings
     Bidirectional(LSTM(32, return_sequences=True)),  # BiLSTM Layer
