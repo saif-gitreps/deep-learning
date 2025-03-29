a436db7bbc1d360682c7cc615c544d60b5b2b0a1
Sf <110230611+saif-gitreps@users.noreply.github.com>
2025-03-29 23:48:23 +0600

    Update example-bilstm.py

--- BiLSTM/example-bilstm.py
+++ BiLSTM/example-bilstm.py
@@ -2,7 +2,7 @@ import numpy as np
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Bidirectional, LSTM, Dense, Embedding
 
-# Sample Data (Batch of 3 sequences, each with 5 words, vocab size 1000)
+#Sample Data (Batch of 3 sequences, each with 5 words, vocab size 1000)
 
 model = Sequential([
     Embedding(input_dim=1000, output_dim=64, input_length=5),  # Word embeddings
@@ -10,4 +10,5 @@ model = Sequential([
     Dense(10, activation="softmax")  # Output Layer
 ])
 
+#checkung summary
 model.summary()
